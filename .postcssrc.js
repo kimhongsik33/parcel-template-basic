9df8bf0c67fec3eb9d65978ae02bbca9e967a526
kim.hongsik33 <scubidu33@gmail.com>
2021-09-20 13:27:44 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,12 @@
+// ESM : General
+// CommonJS : NodeJs
+
+//import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+//export
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
